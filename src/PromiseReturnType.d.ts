f3476cf80a02006793c886fd5bc07e55587041be
xiBread <oscar.leevermeren@gmail.com>
2021-03-11 17:30:44 -0500

    feat: add PromiseReturnType

---
+++ src/PromiseReturnType.d.ts
@@ -0,0 +1,10 @@
+import { ExtendSelf } from './__internal__';
+
+type Resolved<T> = T extends Promise<infer U>
+	? {
+			0: Resolved<U>;
+			1: U;
+	  }[T extends Promise<unknown> ? 0 : 1]
+	: T;
+
+export type PromiseReturnType<T extends (...args: any[]) => Promise<unknown>> = ExtendSelf<T, Resolved<ReturnType<T>>>;
