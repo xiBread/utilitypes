2a70d37075d88af68ecf32140a80e3d04c6673d3
K. Akatsuki <thevortexspirit@gmail.com>
2021-03-19 18:01:02 -0400

    fix: imports

--- src/number/BitXor.ts
+++ src/number/BitXor.ts
@@ -1,4 +1,3 @@
-import { BitAnd } from './BitAnd';
-import { BitOr } from './BitOr';
+import type { BitAnd, BitNot, BitOr } from '../';
 
 export type BitXor<X, Y> = BitOr<BitAnd<X, BitNot<Y>>, BitAnd<BitNot<X>, Y>>;
