9e90987976cd09648164f0ab95869d19ac86e031
xiBread <oscar.leevermeren@gmail.com>
2021-03-19 17:58:20 -0400

    refactor: simplify Filter condition

--- src/array/Filter.ts
+++ src/array/Filter.ts
@@ -1,9 +1,7 @@
 /**
  * Constructs a tuple type by filtering `U` from `T`.
  */
-export type Filter<T extends unknown[], U> = T extends []
-	? []
-	: T extends [infer F, ...infer R]
+export type Filter<T extends unknown[], U> = T extends [infer F, ...infer R]
 	? F extends U
 		? Filter<R, U>
 		: [F, ...Filter<R, U>]
