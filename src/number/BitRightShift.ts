b890f699fc0e3029298e5c31f9afe5268fcf76fa
K. Akatsuki <thevortexspirit@gmail.com>
2021-03-19 16:53:12 -0400

    feat(Number): add Bitwise operator types

---
+++ src/number/BitRightShift.ts
@@ -0,0 +1,5 @@
+import { Binary } from './';
+
+type RightShift<N> = N extends `${infer T}${infer U}` ? (T extends Binary ? `${T}${RightShift<U>}` : never) : never;
+
+export type BitRightShift<N> = `0${RightShift<N>}`;
--- src/number/index.ts
+++ src/number/index.ts
@@ -1,1 +1,9 @@
+export * from './BitAnd';
+export * from './BitLeftShift';
+export * from './BitNot';
+export * from './BitOr';
+export * from './BitRightShift';
+export * from './BitXor';
 export * from './Range';
+
+export type Binary = '0' | '1';
