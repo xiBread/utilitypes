b4a6a92f96a637a4c65bf9623a628be565521939
xiBread <oscar.leevermeren@gmail.com>
2021-03-19 17:53:05 -0400

    feat(Array): add Push and Unshift

---
+++ src/array/Push.ts
@@ -0,0 +1,13 @@
+/**
+ * Constructs a tuple type by appending `U` to the end of `T`.
+ *
+ * @example
+ * ```ts
+ * type T0 = Push<[1, 2, 3], 4>;
+ * //	^ = type T0 = [1, 2, 3, 4];
+ *
+ * type T1 = Push<T0, [5, 6]>;
+ * //	^ = type T1 = [1, 2, 3, 4, 5, 6];
+ * ```
+ */
+export type Push<T extends unknown[], U> = U extends unknown[] ? [...T, ...U] : [...T, U];
