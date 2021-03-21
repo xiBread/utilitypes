e3925262c810d9970a4c83a9d0f9f6f59c8098e8
xiBread <oscar.leevermeren@gmail.com>
2021-03-20 21:45:18 -0400

    refactor: use extra helper

--- src/string/Path.ts
+++ src/string/Path.ts
@@ -1,9 +1,11 @@
+type Child<T, K extends keyof T> = Exclude<keyof T[K], keyof any[]>;
+
 /* prettier-ignore */
 type PathString<T, K extends keyof T> = K extends string
 	? T[K] extends Record<string, any>
 		?
-			| `${K}.${PathString<T[K], Exclude<keyof T[K], keyof any[]>> & string}`
-			| `${K}.${Exclude<keyof T[K], keyof any[]> & string}`
+			| `${K}.${PathString<T[K], Child<T, K>> & string}`
+			| `${K}.${Child<T, K> & string}`
 		: never
 	: never;
 
