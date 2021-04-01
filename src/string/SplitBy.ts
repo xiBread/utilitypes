2a6e5396e8a0a83fb0e33e9da2f36a906613fd37
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-01 17:19:51 -0400

    chore(String): add example to `SplitBy`

--- src/string/SplitBy.ts
+++ src/string/SplitBy.ts
@@ -2,5 +2,11 @@ import type { Filter, Split } from '../';
 
 /**
  * Constructs a tuple type, excluding `U`, by splitting `S` by `U`.
+ *
+ * @example
+ * ```ts
+ * type T0 = SplitBy<'aaa7bbb7ccc', '7'>;
+ * //	^ = type T0 = ["aaa", "bbb", "ccc"]
+ * ```
  */
 export type SplitBy<S extends string, U extends string> = Filter<Split<S, U>, U>;
