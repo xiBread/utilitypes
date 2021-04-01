79ed68e9ed7a8bbfdbbc322e3121c790205f2e82
xiBread <oscar.leevermeren@gmail.com>
2021-04-01 17:17:11 -0400

    refactor(String): simplify type logic

--- src/string/Split.ts
+++ src/string/Split.ts
@@ -1,14 +1,12 @@
 /**
  * Constructs a tuple type, including `U`, by splitting `S` by `U`.
+ *
+ * @example
+ * ```ts
+ * type T0 = Split<'aaa7bbb7ccc', '7'>;
+ * //	^ = type T0 = ["aaa", "7", "bbb", "7", "ccc"]
+ * ```
  */
-export type Split<S extends string, U extends string> = S extends ''
-	? []
-	: S extends `${infer F}${U}${infer R}`
-	? S extends `${F}${infer D}${R}`
-		? D extends U
-			? S extends `${infer F}${D}${infer R}`
-				? [...Split<F, U>, D, ...Split<R, U>]
-				: never
-			: never
-		: never
+export type Split<S extends string, U extends string> = S extends `${infer F}${U}${infer R}`
+	? [...Split<F, U>, U, ...Split<R, U>]
 	: [S];
