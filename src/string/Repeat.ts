a26076a29e62463a158374ad9abd51691508565a
xiBread <oscar.leevermeren@gmail.com>
2021-04-01 17:15:06 -0400

    feat(String): add `Replace` type

---
+++ src/string/Repeat.ts
@@ -0,0 +1,12 @@
+import type { FixedTuple, Join } from '../array';
+
+/**
+ * Constructs a string from `S` containing `N` copies concatenated together.
+ *
+ * @example
+ * ```ts
+ * type T0 = Repeat<'Hi', 5>;
+ * //	^ = type T0 = 'HiHiHiHiHi'
+ * ```
+ */
+export type Repeat<S extends string, N extends number> = Join<FixedTuple<S, N>, ''>;
