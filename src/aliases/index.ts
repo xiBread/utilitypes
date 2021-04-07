7817298d591b67e5d6f853aa63b3501e7295daeb
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-07 18:00:43 -0400

    feat(Aliases): add `{Lower,Upper}caseCharacter` unions

--- src/aliases/index.ts
+++ src/aliases/index.ts
@@ -1,5 +1,7 @@
 export * from './LiteralPrimitive';
+export * from './LowercaseCharacter';
 export * from './Maybe';
 export * from './Nil';
 export * from './Nullable';
 export * from './Primitive';
+export * from './UppercaseCharacter';
