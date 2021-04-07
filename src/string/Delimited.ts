88b30139fb32937bf04f454f9faf23bcbbe80038
xiBread <oscar.leevermeren@gmail.com>
2021-04-07 18:59:51 -0400

    fix(String): fix deep instantiation

--- src/string/Delimited.ts
+++ src/string/Delimited.ts
@@ -1,23 +1,12 @@
-import type { ExtendSelf, Separators, Split } from '../';
+import type { Delimiter, SplitWith, UppercaseCharacter } from '../';
 
-/* prettier-ignore */
-type Characters =
-	| 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G'
-	| 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N'
-	| 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U'
-	| 'V' | 'W' | 'X' | 'Y' | 'Z';
-
-type Join<T extends unknown[], U extends string, S extends string, D extends string> = T extends [
-	`${infer F}`,
-	...infer R
-]
-	? `${F extends U ? D : F extends S ? `${D}${Lowercase<F>}` : F}${Join<R, U, S, D>}`
+type Join<T extends unknown[], U extends string> = T extends [`${infer F}`, ...infer R]
+	? `${F extends Delimiter ? U : F extends UppercaseCharacter ? `${U}${Lowercase<F>}` : F}${Join<R, U>}`
 	: '';
 
 /**
  * Converts a string by delimiting each compound word in `S` by `U`.
  */
-export type Delimited<S extends string, U extends string> = ExtendSelf<
-	S,
-	Join<Split<S, Separators | Characters>, Separators, Characters, U>
->;
+export type Delimited<S extends string, U extends string> = string extends U
+	? S
+	: Join<SplitWith<S, Delimiter | UppercaseCharacter>, U>;
