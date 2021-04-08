386be35efc68ff019159cca9c1498d77950503b8
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-08 15:41:13 -0400

    feat(Aliases): add `Nillable` type

--- src/aliases/index.ts
+++ src/aliases/index.ts
@@ -2,6 +2,7 @@ export * from './LiteralPrimitive';
 export * from './LowercaseCharacter';
 export * from './Maybe';
 export * from './Nil';
+export * from './Nillable';
 export * from './Nullable';
 export * from './Primitive';
 export * from './UppercaseCharacter';
