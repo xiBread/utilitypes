07f30eed651f3f06ef5a209d075bd595f5bd0d89
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-15 16:41:03 -0400

    refactor(String): improve `Trim{End,Start}` logic

--- src/string/TrimStart.ts
+++ src/string/TrimStart.ts
@@ -1,3 +1,5 @@
+import type { Whitespace } from '../aliases';
+
 /**
  * Removes whitespace from the start of a string.
  *
@@ -7,4 +9,4 @@
  * //	^ = type T0 = 'Hello   '
  * ```
  */
-export type TrimStart<S extends string> = S extends ` ${infer T}` ? TrimStart<T> : S;
+export type TrimStart<S extends string> = S extends `${Whitespace}${infer T}` ? TrimStart<T> : S;
