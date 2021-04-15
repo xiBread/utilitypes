d483bed0541baaba61104da44aad2e3438dbbd55
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-15 18:03:26 -0400

    refactor(String): add brace syntax support to `QueryParameters`

--- src/string/QueryParameters.ts
+++ src/string/QueryParameters.ts
@@ -1,7 +1,11 @@
-type Extract<T extends string> = T extends `${infer S}:${infer P}/${infer R}`
+type Extract<T> = T extends `${infer F}{${infer P}}${infer R}`
+	? R extends `/${infer R}`
+		? { [K in P | keyof QueryParameters<R>]: string }
+		: { [K in P]: string }
+	: T extends `${infer F}:${infer P}/${infer R}`
 	? { [K in P | keyof QueryParameters<R>]: string }
-	: T extends `${infer S}:${infer P}`
+	: T extends `${infer F}:${infer P}`
 	? { [K in P]: string }
-	: {};
+	: Record<string, never>;
 
 export type QueryParameters<T extends string> = string extends T ? Record<string, string> : Extract<T>;
