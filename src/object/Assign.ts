39782cc7e5f5c7335c93cfcf3f576b27b5eafbe6
xiBread <oscar.leevermeren@gmail.com>
2021-04-15 16:32:03 -0400

    refactor(Object): support multiple sources for `Assign`

--- src/object/Assign.ts
+++ src/object/Assign.ts
@@ -1,30 +1,35 @@
 import type { Mapped } from '../';
 
+type Merge<T, U> = Mapped<Omit<T, Extract<keyof T, keyof U>>> & U;
+
 /**
  * Constructs a type with each property in `U` assigned to `T`.
  *
  * @example
  * ```ts
  * interface Target {
- * 	a: number;
- * 	b: boolean[];
- * 	c: string;
+ * 	a: string;
+ * 	b: number;
  * }
  *
- * interface Source {
- * 	c: string[];
- * 	d: bigint;
- * 	e?: 'y' | 'n';
- * }
+ * type Sources = [{ b: { c: any[] } }, { d: boolean }]
  *
- * type Assigned = Assign<Target, Source>;
- * //		  ^ = type Assigned = {
- * //			  	a: number;
- * //			  	b: boolean[];
- * //			  	c: string[];
- * //			  	d: bigint;
- * //			  	e?: 'y' | 'n';
- * // 		 	}
+ * type T0 = Assign<Target, { a: { b: number } }>;
+ * //	^ = type T0 = {
+ * //			a: {
+ * //				b: number;
+ * //			};
+ * //			b: number;
+ * //		}
+ *
+ * type T1 = Assign<Target, Sources>;
+ * //	^ = type T1 = {
+ * //			a: string;
+ * //			b: {
+ * //				c: any[];
+ * //			};
+ * //			d: boolean;
+ * //		}
  * ```
  */
-export type Assign<T, U> = Mapped<Omit<T, Extract<keyof T, keyof U>>> & U;
+export type Assign<T, U> = U extends [infer F, ...infer R] ? Assign<Merge<T, F>, R> : U extends [] ? T : Merge<T, U>;
