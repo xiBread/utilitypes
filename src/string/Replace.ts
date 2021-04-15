cda4b90661bf6f7f56df443f88d2ba635318e1a3
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-15 17:47:45 -0400

    refactor(String): make `Replace` replace once

--- src/string/Replace.ts
+++ src/string/Replace.ts
@@ -1,10 +1,6 @@
-import type { CastAs } from '../';
-
 /**
- * Replaces all occurances of `T` in `S` with `U`.
+ * Replaces the first occurance of `T` in `S` with `U`.
  */
 export type Replace<S extends string, T extends string, U extends string> = S extends `${infer F}${T}${infer R}`
-	? Replace<`${F}${U}${R}`, T, U>
-	: S extends infer T
-	? CastAs<T, string>
-	: never;
+	? `${F}${U}${R}`
+	: S;
