b7082453cce21f83f9c39564f20342c38caebd39
xiBread <oscar.leevermeren@gmail.com>
2021-04-15 17:39:33 -0400

    refactor(Common): use `Omit` for consistency

--- src/common/OmitStrict.ts
+++ src/common/OmitStrict.ts
@@ -1,4 +1,4 @@
 /**
  * A type-safe version of TypeScript's built-in `Omit`.
  */
-export type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type OmitStrict<T, K extends keyof T> = Omit<T, K>;
