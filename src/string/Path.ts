7249d9da64843fbd7d37cd2e1ed5a28ebb0ec8aa
xiBread <oscar.leevermeren@gmail.com>
2021-04-15 18:27:04 -0400

    refactor(String): remove redundancy

--- src/string/Path.ts
+++ src/string/Path.ts
@@ -1,11 +1,11 @@
-type Child<T, K extends keyof T> = Exclude<keyof T[K], keyof any[]>;
+type Child<T, K extends keyof T> = Exclude<keyof T[K], keyof any[]> & string;
 
 /* prettier-ignore */
 type PathString<T, K extends keyof T> = K extends string
 	? T[K] extends Record<string, any>
 		?
-			| `${K}.${PathString<T[K], Child<T, K>> & string}`
-			| `${K}.${Child<T, K> & string}`
+			| `${K}.${PathString<T[K], Child<T, K>>}`
+			| `${K}.${Child<T, K>}`
 		: never
 	: never;
 
