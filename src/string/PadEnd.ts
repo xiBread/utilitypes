e738b8fdbe35dc3fe2a482e8ce194277767777f1
xiBread <oscar.leevermeren@gmail.com>
2021-04-16 16:19:30 -0400

    refactor(String): simplify `Pad{End,Start}` logic

--- src/string/PadEnd.ts
+++ src/string/PadEnd.ts
@@ -1,6 +1,17 @@
-import type { FixedTuple, Join, Slice, Split } from '../';
+import type { Length } from '../';
 
-export type PadEnd<S extends string, N extends number, T extends string = ' '> = Join<
-	[S, ...Slice<FixedTuple<T, N>, Split<S, ''>['length']>],
-	''
->;
+/**
+ * Pads the ends of a string with `T` until the total length of `S` is equal to `N`.
+ *
+ * @example
+ * ```ts
+ * type T0 = PadEnd<'Breaded Mushrooms', 25, '.'>;
+ * //	^ = type T0 = "Breaded Mushrooms........"
+ *
+ * type T1 = PadEnd<'200', 5>;
+ * //	^ = type T1 = "200 "
+ * ```
+ */
+export type PadEnd<S extends string, N extends number, T extends string = ' '> = Length<S> extends N
+	? S
+	: PadEnd<`${S}${T}`, N, T>;
