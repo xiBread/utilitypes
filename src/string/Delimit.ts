54983e33e832ace29177f8423d911476d081693c
xiBread <oscar.leevermeren@gmail.com>
2021-04-16 17:53:21 -0400

    fix(String): use old `Split` type for `Delimit`

--- src/string/Delimit.ts
+++ src/string/Delimit.ts
@@ -1,4 +1,16 @@
-import type { Delimiter, SplitWith, UppercaseCharacter } from '../';
+import type { Delimiter, UppercaseCharacter } from '../';
+
+type Split<S extends string, U extends string> = S extends ''
+	? []
+	: S extends `${infer F}${U}${infer R}`
+	? S extends `${F}${infer T}${R}`
+		? T extends U
+			? S extends `${infer F}${T}${infer R}`
+				? [...Split<F, U>, T, ...Split<R, U>]
+				: never
+			: never
+		: never
+	: [S];
 
 type Join<T extends unknown[], U extends string> = T extends [`${infer F}`, ...infer R]
 	? `${F extends Delimiter ? U : F extends UppercaseCharacter ? `${U}${Lowercase<F>}` : F}${Join<R, U>}`
@@ -6,7 +18,15 @@ type Join<T extends unknown[], U extends string> = T extends [`${infer F}`, ...i
 
 /**
  * Converts a string by delimiting each compound word in `S` by `U`.
+ *
+ * @example
+ * ```ts
+ * type T0 = Delimit<'someLongWord'>;
+ * //	^ = type T0 = 'some-long-word'
+ *
+ * type T1 = Delimit<'hello world', '_'>;
+ * //	^ = type T1 = 'hello_world'
  */
-export type Delimit<S extends string, U extends string> = string extends U
+export type Delimit<S extends string, U extends string = '-'> = string extends U
 	? S
-	: Join<SplitWith<S, Delimiter | UppercaseCharacter>, U>;
+	: Join<Split<S, Delimiter | UppercaseCharacter>, U>;
