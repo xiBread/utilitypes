1b71040359ac228026d9740974bd188ec09b1e8f
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-16 16:19:30 -0400

    refactor(String): simplify `Pad{End,Start}` logic

--- src/string/PadStart.ts
+++ src/string/PadStart.ts
@@ -1,6 +1,17 @@
-import type { FixedTuple, Join, Slice, Split } from '../';
+import type { Length } from '../';
 
-export type PadStart<S extends string, N extends number, T extends string = ' '> = Join<
-	[...Slice<FixedTuple<T, N>, Split<S, ''>['length']>, S],
-	''
->;
+/**
+ * Pads the start of a string with `T` until the total length of `S` is equal to `N`.
+ *
+ * @example
+ * ```ts
+ * type T0 = PadStart<'5', 3, '0'>;
+ * //	^ = type T0 = "005"
+ *
+ * type T1 = PadStart<'Computer', 12>;
+ * //	^ = type T1 = "    Computer"
+ * ```
+ */
+export type PadStart<S extends string, N extends number, T extends string = ' '> = Length<S> extends N
+	? S
+	: PadStart<`${T}${S}`, N, T>;
