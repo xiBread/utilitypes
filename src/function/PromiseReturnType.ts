551d25159c01d977d78f803c3674567d9b1ab33b
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-16 16:31:39 -0400

    refactor(Function): simplify `PromiseReturnType` logic

--- src/function/PromiseReturnType.ts
+++ src/function/PromiseReturnType.ts
@@ -1,19 +1,17 @@
-import type { ExtendSelf } from '../';
-
-type Resolved<T> = T extends Promise<infer U> ? (T extends Promise<unknown> ? Resolved<U> : U) : T;
+type Resolved<T> = T extends Promise<infer U> ? Resolved<U> : T;
 
 /**
  * Constructs a type consisting of the return type of a `Promise`.
  *
  * @example
  * ```ts
- * declare function f1(): Promise<Promise<string>>>
+ * declare function f1(): Promise<Promise<string>>;
  *
  * type T0 = PromiseReturnType<typeof f1>;
  * //	^ = type T0 = string
  *
- * type T1 = PromiseReturnType<() => Promise<string[]>>
+ * type T1 = PromiseReturnType<() => Promise<string[]>>;
  * //	^ = type T1 = string[]
  * ```
  */
-export type PromiseReturnType<T extends (...args: any[]) => Promise<unknown>> = ExtendSelf<T, Resolved<ReturnType<T>>>;
+export type PromiseReturnType<T extends (...args: any[]) => Promise<unknown>> = Resolved<ReturnType<T>>;
