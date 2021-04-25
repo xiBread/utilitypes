b1e7303db0ba7a35b103489a7c4ca805693575fd
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-24 21:39:07 -0400

    revert(String): re-rename `PathParameters`

--- src/string/PathParameters.ts
+++ src/string/PathParameters.ts
@@ -1,11 +1,11 @@
 type Extract<T> = T extends `${infer F}{${infer P}}${infer R}`
 	? R extends `/${infer R}`
-		? { [K in P | keyof QueryParameters<R>]: string }
+		? { [K in P | keyof PathParameters<R>]: string }
 		: { [K in P]: string }
 	: T extends `${infer F}:${infer P}/${infer R}`
-	? { [K in P | keyof QueryParameters<R>]: string }
+	? { [K in P | keyof PathParameters<R>]: string }
 	: T extends `${infer F}:${infer P}`
 	? { [K in P]: string }
 	: Record<string, never>;
 
-export type QueryParameters<T extends string> = string extends T ? Record<string, string> : Extract<T>;
+export type PathParameters<T extends string> = string extends T ? Record<string, string> : Extract<T>;
