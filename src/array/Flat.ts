1d4ba5e6d72d0f864d0f44807fb3d223c097ea68
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-25 16:20:05 -0400

    refactor(Flat): allow `void` to act as `Infinity`

--- src/array/Flat.ts
+++ src/array/Flat.ts
@@ -4,9 +4,15 @@ type Concat<T extends unknown[], U extends unknown[], N extends number> = T['len
 	? U
 	: Concat<T, T[N] extends unknown[] ? [...U, ...T[N]] : [...U, T[N]], Add<N, 1>>;
 
-type Recurse<T extends unknown[], N extends number, U extends number> = U extends N
+type Recurse<T extends unknown[], N extends number, U extends number = 0> = U extends N
 	? T
 	: Recurse<Concat<T, [], 0>, N, Add<U, 1>>;
+
+type Flatten<T extends unknown[]> = T extends [infer F, ...infer R]
+	? F extends unknown[]
+		? [...Flatten<F>, ...Flatten<R>]
+		: [F, ...Flatten<R>]
+	: [];
 
 /**
  * Constructs an array type with all sub-arrays concatenated up to the specified depth.
@@ -16,11 +22,19 @@ type Recurse<T extends unknown[], N extends number, U extends number> = U extend
  * type T0 = Flat<[0, 1, [2, 3], 4]>;
  * //	^ = type T0 = [0, 1, 2, 3, 4]
  *
- * type T1 = Flat<[0, 1, 2, [3, [4, 5]]], 2>;
+ * type T1 = Flat<[0, 1, 2, [3, [4, 5]]], void>;
  * //	^ = type T1 = [0, 1, 2, 3, 4, 5]
  *
  * type T2 = Flat<[0, [[[1, [2]], [3, [4, [5]]], 6], [7, 8]]], 3>;
  * //	^ = type T2 = [0, 1, [2], 3, [4, [5]], 6, 7, 8]
  * ```
  */
-export type Flat<T extends unknown[], N extends number = 1> = N extends 0 ? T : number extends N ? T : Recurse<T, N, 0>;
+export type Flat<T extends unknown[], N extends void | number = 1> = N extends void
+	? Flatten<T>
+	: N extends number
+	? N extends 0
+		? T
+		: number extends N
+		? T
+		: Recurse<T, N>
+	: [];
