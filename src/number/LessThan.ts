112ecc6705b0012c8ef5cc2d5af2c9b30e102510
xiBread <oscar.leevermeren@gmail.com>
2021-04-25 19:33:49 -0400

    refactor(Number): use `Compare` for `{Greater,Less}Than`

--- src/number/LessThan.ts
+++ src/number/LessThan.ts
@@ -1,11 +1,3 @@
-import type { FixedTuple, Initial } from '../';
+import type { Compare } from '../';
 
-type Decrement<N extends number> = N extends 0 ? N : Initial<FixedTuple<unknown, N>>['length'];
-
-export type LessThan<X extends number, Y extends number> = X extends Y
-	? false
-	: Y extends 0
-	? false
-	: X extends 0
-	? true
-	: LessThan<Decrement<X>, Decrement<Y>>;
+export type LessThan<X extends number, Y extends number> = Compare<X, Y> extends -1 ? true : false;
