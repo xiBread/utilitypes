4d1d82e36846c207365583967de076799e79e01e
xiBread <oscar.leevermeren@gmail.com>
2021-04-24 22:20:21 -0400

    refactor(Slice): allow negative indices

--- src/array/Slice.ts
+++ src/array/Slice.ts
@@ -1,13 +1,50 @@
-type Start<T extends unknown[], N extends number, S extends unknown[] = []> = S['length'] extends N
-	? T
-	: T extends [infer U, ...infer R]
-	? Start<R, N, [U, ...S]>
-	: [];
+import type { Absolute, Sign } from '../';
 
-type End<T extends unknown[], N extends number> = T['length'] extends N
+type Start<T extends unknown[], N extends number, U = T> = Sign<N> extends -1
+	? T extends [infer F, ...infer R]
+		? R['length'] extends Absolute<N>
+			? [F]
+			: Start<R, N> extends infer R
+			? R extends []
+				? []
+				: R extends unknown[]
+				? [F, ...R]
+				: never
+			: never
+		: []
+	: T['length'] extends N
 	? T
-	: T extends [...infer U, infer R]
-	? End<U, N>
+	: T extends []
+	? U
+	: T extends [...infer F, unknown]
+	? Start<F, N, U>
 	: never;
 
-export type Slice<T extends unknown[], X extends number, Y extends number = T['length']> = Start<End<T, Y>, X>;
+type End<T extends unknown[], N extends number> = T extends [...Start<T, N>, ...infer R] ? R : never;
+
+/**
+ * Constructs an array type by selecting each element in `T` from `X` to but not including `Y`.
+ * 
+ * @example
+ * ```ts
+ * type Animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+ * 
+ * type T0 = Slice<Animals, 2>;
+ * //	^ = type T0 = ["camel", "duck", "elephant"]
+ * 
+ * type T1 = Slice<Animals, 0, 2>;
+ * //	^ = type T1 = ["ant", "bison"]
+ * 
+ * type T2 = Slice<Animals, -2>;
+ * //	^ = type T2 = ["duck", "elephant"]
+
+ * type T3 = Slice<Animals, -4, -1>;
+ * //	^ = type T3 = ["bison", "camel", "duck"]
+ */
+export type Slice<T extends unknown[], X extends number = 0, Y extends number = T['length']> = T extends [
+	...Start<T, X>,
+	...infer U,
+	...End<T, Y>
+]
+	? U
+	: [];
