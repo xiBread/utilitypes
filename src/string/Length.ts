2a73588769e4925691e9a7661105210e2b44e05b
xiBread <oscar.leevermeren@gmail.com>
2021-04-25 21:08:40 -0400

    refactor(Length): allow deeper recursion

--- src/string/Length.ts
+++ src/string/Length.ts
@@ -1,3 +1,12 @@
-import type { Split } from '../';
+import type { FixedTuple } from '../';
 
-export type Length<S extends string> = Split<S, ''>['length'];
+type Split<
+	S extends string,
+	T extends unknown[] = []
+> = S extends `${infer $0}${infer $1}${infer $2}${infer $3}${infer $4}${infer $5}${infer $6}${infer $7}${infer $8}${infer $9}${infer R}`
+	? Split<R, [...FixedTuple<unknown, 10>, ...T]>
+	: S extends `${infer $0}${infer R}`
+	? Split<R, [unknown, ...T]>
+	: T;
+
+export type Length<S extends string> = Split<S>['length'];
