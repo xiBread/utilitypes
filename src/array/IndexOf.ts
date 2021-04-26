e0d3537e2f2dcf03177ebed3941684984175be04
xiBread <oscar.leevermeren@gmail.com>
2021-04-26 18:29:55 -0400

    refactor(IndexOf): return number

--- src/array/IndexOf.ts
+++ src/array/IndexOf.ts
@@ -1,4 +1,4 @@
-import type { Invert, Range, RecordOf, TupleOf } from '../';
+import type { CastAs, Invert, ParseInt, Range, RecordOf, TupleOf } from '../';
 
 type MapArray<T extends readonly unknown[], N extends number> = Invert<
 	Omit<RecordOf<T>, N extends 0 ? '' : number extends N ? '' : `${Range<0, N>}`>
@@ -12,17 +12,17 @@ type MapArray<T extends readonly unknown[], N extends number> = Invert<
  * type Colors = ['green', 'red', 'blue', 'yellow', 'red', 'pink'];
  *
  * type T0 = IndexOf<Colors, 'red'>;
- * //	^ = type T0 = "1"
+ * //	^ = type T0 = 1
  *
  * type T1 = IndexOf<Colors, 'red', 3>;
- * //	^ = type T1 = "4"
+ * //	^ = type T1 = 4
  *
  * type T2 = IndexOf<Colors, 'orange'>;
- * //	^ = type T2 = "-1"
+ * //	^ = type T2 = -1
  * ```
  */
 export type IndexOf<T extends readonly unknown[], U, N extends number = 0> = MapArray<T, N> extends infer K
 	? U extends keyof K
-		? TupleOf<K[U]>[0]
-		: '-1'
+		? ParseInt<CastAs<TupleOf<K[U]>[0], `${number}`>>
+		: -1
 	: never;
