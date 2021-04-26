aabe3e05f9dc386639cab0a766f06c80fd090792
xiBread <oscar.leevermeren@gmail.com>
2021-04-26 19:04:52 -0400

    refactor(Sign): use `IsNegative`

--- src/number/Sign.ts
+++ src/number/Sign.ts
@@ -1,1 +1,18 @@
-export type Sign<N extends number> = N extends 0 ? 0 : `${N}` extends `-${infer T}` ? -1 : 1;
+import type { IsNegative } from '../';
+
+/**
+ * Returns either a `1` or `-1` indicating the sign of `N` or `0` if `N` is `0`.
+ *
+ * @example
+ * ```ts
+ * type T0 = Sign<5>;
+ * //	^ = type T0 = 1
+ *
+ * type T1 = Sign<-11>;
+ * //	^ = type T1 = -1
+ *
+ * type T2 = Sign<0>;
+ * //	^ = type T2 = 0
+ * ```
+ */
+export type Sign<N extends number> = N extends 0 ? 0 : IsNegative<N> extends true ? -1 : 1;
