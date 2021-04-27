146dbe537771408eb1b919d5da4e0a3ce91e57ce
xiBread <oscar.leevermeren@gmail.com>
2021-04-27 19:24:42 -0400

    refactor(Entries): rewrite; only allow objects or arrays

--- src/array/Entries.ts
+++ src/array/Entries.ts
@@ -1,24 +1,18 @@
-import type { ExtendSelf } from '../';
-
-/* prettier-ignore */
-type EntryPairing<T> = T extends object
-	? readonly (readonly [keyof T, T[keyof T]])[]
-	: T extends unknown[]
-		? readonly [number, T[number]]
-		: T extends Set<unknown>
-			? T extends Set<infer V>
-				? readonly [V, V]
-				: never
-			: T extends Map<unknown, unknown>
-		? [
-			T extends Map<infer K, unknown> 
-				? K : never, 
-			T extends Map<infer V, unknown> 
-				? V : never
-		]
-	: never;
+import type { RecordOf, TupleOf, Zip } from '../';
 
 /**
- * Constructs a tuple type from `T` whose entries are key-value pairs.
+ * Constructs an array type whose entries are key-value pairs from `T`.
+ *
+ * @example
+ * ```ts
+ * type T0 = Entries<{ a: 1, b: 2, c: 3 }>;
+ * //	^ = type T0 = [["a", 1], ["b", 2], ["c", 3]]
+ *
+ * type T1 = Entries<['a', 'b', 'c']>;
+ * //	^ = type T1 = [["0", "a"], ["1", "b"], ["2", "c"]]
  */
-export type Entries<T> = ExtendSelf<T, EntryPairing<T>>;
+export type Entries<T> = T extends readonly unknown[]
+	? Entries<RecordOf<T>>
+	: T extends Record<infer K, infer V>
+	? Zip<[TupleOf<K>, TupleOf<V>]>
+	: [];
