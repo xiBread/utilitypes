40f2fc335286ed4f7789befd420caaec47df4edf
xiBread <oscar.leevermeren@gmail.com>
2021-04-27 20:06:37 -0400

    fix({Omit,Pick}AllBy): update import

--- src/object/OmitAllBy.ts
+++ src/object/OmitAllBy.ts
@@ -1,6 +1,6 @@
-import type { Values } from '../';
+import type { KeyBy } from '../';
 
 /**
  * Constructs a type by omitting each property in `T` based on `U`.
  */
-export type OmitAllBy<T, U> = Omit<T, Values<T, U>>;
+export type OmitAllBy<T, U> = Omit<T, KeyBy<T, U>>;
