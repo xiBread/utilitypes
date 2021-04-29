97520e6b8dcb45615b07a9d4f424c60e8eb8cc55
xiBread <oscar.leevermeren@gmail.com>
2021-04-29 18:07:34 -0400

    chore: specify type-safety in descriptions

--- src/union/ExcludeStrict.ts
+++ src/union/ExcludeStrict.ts
@@ -1,4 +1,5 @@
 /**
- * A type-safe version of TypeScript's built-in `Exclude`.
+ * A type-safe version of TypeScript's built-in `Exclude` that allows IntelliSense to provide
+ * autocompletion for `U`.
  */
 export type ExcludeStrict<T, U extends T> = Exclude<T, U>;
