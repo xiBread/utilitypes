4749da618ae552e2068bc7e05042b60c030fcd2c
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-29 18:04:14 -0400

    refactor(LiteralUnion): support objects

--- src/union/LiteralUnion.ts
+++ src/union/LiteralUnion.ts
@@ -1,1 +1,18 @@
-export type LiteralUnion<T extends string> = T | (string & {});
+/**
+ * A type-safe version of literal unions that allows IntelliSense to provide autocompletion for `T`
+ * while also allowing `U`.
+ *
+ * @example
+ * ```ts
+ * type T0 = 'foo' | 'bar'
+ *
+ * const x: T0 | string = ' ';
+ * //					  ^ Nothing
+ *
+ * const y: LiteralUnion<T0> = ' ';
+ * //						   ^ foo
+ * //							 bar
+ * //							 <string>
+ * ```
+ */
+export type LiteralUnion<T extends U, U = string> = T | (Pick<U, never> & { $?: never });
