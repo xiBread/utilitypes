6cd36d56683acf67b93807bf3bc7ed1a71bd617c
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-29 18:07:34 -0400

    chore: specify type-safety in descriptions

--- src/object/OmitStrict.ts
+++ src/object/OmitStrict.ts
@@ -1,4 +1,5 @@
 /**
- * A type-safe version of TypeScript's built-in `Omit`.
+ * A type-safe version of TypeScript's built-in `Omit` that allows IntelliSense to provide
+ * autocompletion for `K`.
  */
 export type OmitStrict<T, K extends keyof T> = Omit<T, K>;
--- src/union/ExtractStrict.ts
+++ src/union/ExtractStrict.ts
@@ -1,4 +1,5 @@
 /**
- * A type-safe version of TypeScript's built-in `Extract`.
+ * A type-safe version of TypeScript's built-in `Extract` that allows IntelliSense to provide
+ * autocompletion for `U`.
  */
 export type ExtractStrict<T, U extends T> = Exclude<T, U>;
