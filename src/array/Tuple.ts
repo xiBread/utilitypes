6b174a092d2e8e860e6dbc1ce543bfc5079ed3c4
xiBread <oscar.leevermeren@gmail.com>
2021-05-01 20:52:52 -0400

    refactor(Tuple): rename params & helpers

--- src/array/Tuple.ts
+++ src/array/Tuple.ts
@@ -1,19 +1,19 @@
-type Populate<N extends number, R extends never[][]> = R[0][N] extends never
-	? R
-	: Populate<N, [[...R[0], ...R[0]], ...R]>;
+type Distribute<T extends never[][], N extends number> = T[0][N] extends never
+	? T
+	: Distribute<[[...T[0], ...T[0]], ...T], N>;
 
-type Concat<N extends number, R extends never[][], B extends never[]> = B['length'] extends N
-	? B
-	: [...R[0], ...B][N] extends never
-	? Concat<N, R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never, B>
-	: Concat<N, R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never, [...R[0], ...B]>;
+type Iterate<N extends number, T extends never[][], U extends never[]> = U['length'] extends N
+	? U
+	: [...T[0], ...U][N] extends never
+	? Iterate<N, T extends [T[0], ...infer R] ? (R extends never[][] ? R : never) : never, U>
+	: Iterate<N, T extends [T[0], ...infer R] ? (R extends never[][] ? R : never) : never, [...T[0], ...U]>;
 
 type Replace<T, U> = { [K in keyof T]: U };
 
-type Build<T, N extends number> = {
-	[K in N]: Populate<K, [[never]]> extends infer U
+type Populate<T, N extends number> = {
+	[K in N]: Distribute<[[never]], K> extends infer U
 		? U extends never[][]
-			? Replace<Concat<K, U, []>, T>
+			? Replace<Iterate<K, U, []>, T>
 			: never
 		: never;
 }[N];
@@ -27,4 +27,4 @@ type Build<T, N extends number> = {
  * //	^ = type T0 = [number, number, number]
  * ```
  */
-export type Tuple<T, N extends number> = number extends N ? T[] : Build<T, N>;
+export type Tuple<T, N extends number> = number extends N ? T[] : Populate<T, N>;
