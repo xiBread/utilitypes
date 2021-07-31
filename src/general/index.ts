9e3d15f524d091399a267f3f410abb58ef2bc83f
K. Akatsuki <thevortexspirit@gmail.com>
2021-07-31 16:11:03 -0400

    refactor: make `Length` and `Slice` work for strings & arrays

--- src/general/index.ts
+++ src/general/index.ts
@@ -3,4 +3,6 @@ export * from './IsEqual';
 export * from './IsAny';
 export * from './IsNever';
 export * from './IsTuple';
+export * from './Length';
 export * from './Opaque';
+export * from './Slice';
--- src/internal.ts
+++ src/internal.ts
@@ -1,1 +1,3 @@
 export type ExtendSelf<T, U> = T extends T ? U : never;
+
+export type Indexable = string | readonly unknown[];
