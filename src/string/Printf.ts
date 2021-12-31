930443341549c1a9214f49c3f04d6bcb49a9f43c
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2021-12-31 00:12:09 -0500

    refactor(printf): use type map instead of conditions

--- src/string/Printf.ts
+++ src/string/Printf.ts
@@ -1,28 +1,25 @@
-/* prettier-ignore */
-type Specifier<T> =
-	T extends 'a' ? any :
-	T extends 'b' ? bigint :
-	T extends 'd' ? number :
-	T extends 'i' ? number :
-	T extends 'u' ? number :
-	T extends 'o' ? number :
-	T extends 'x' ? number :
-	T extends 'f' ? number :
-	T extends 'e' ? number :
-	T extends 'c' ? string :
-	T extends 's' ? string : never
+type SpecifierMap = {
+	a: any;
+	b: bigint;
+	c: string;
+	s: string;
+} & {
+	[K in "d" | "i" | "u" | "o" | "x" | "f" | "e"]: number;
+};
+
+type Specifier<T> = T extends keyof SpecifierMap ? SpecifierMap[T] : never;
 
 /**
  * Constructs a tuple type based on each specifier found in `S`.
  *
  * @example
  * ```ts
- * type T0 = Printf<'%i + %i = %i'>
- * //	^ = [number, number, number]
+ * type T0 = Printf<'%i + %i = %i'>;
+ * //	^ type T0 = [number, number, number]
  * ```
  */
-export type Printf<S extends string> = S extends `${infer F}%${infer T}${infer R}`
+export type Printf<S extends string> = S extends `${infer _}%${infer T}${infer R}`
 	? [Specifier<T>, ...Printf<R>]
-	: S extends `${infer F}%${infer T}`
+	: S extends `${infer _}%${infer T}`
 	? [Specifier<T>]
 	: [];
