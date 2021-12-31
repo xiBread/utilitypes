134e79d2ad5ca0383ee834d9b0bffcb68ece6825
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2021-12-30 22:17:15 -0500

    refactor(general): use `unique symbol` for `Opaque` key

--- src/general/Opaque.ts
+++ src/general/Opaque.ts
@@ -1,5 +1,7 @@
+declare const _: unique symbol;
+
 /**
  * Constructs a type by hiding the internal metadata of `T` based on `U`, making `T` incompatible
  * with other types that are seemingly structually identical.
  */
-export type Opaque<T, U> = T & { readonly _: U };
+export type Opaque<T, S> = T & { readonly [_]: S };
