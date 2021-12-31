fd209d8cb58b0bf988831706719e20f3806d593e
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2021-12-30 21:58:48 -0500

    chore(aliases): add mdn link for `primitive`

--- src/aliases/Primitive.ts
+++ src/aliases/Primitive.ts
@@ -1,4 +1,4 @@
 /**
- * A type alias for each primitive data type.
+ * A type alias for each [primitive data type](https://developer.mozilla.org/en-US/docs/Glossary/Primitive).
  */
 export type primitive = string | number | bigint | boolean | undefined | symbol | null;
